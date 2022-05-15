4eea20275ee2999f9605f489230aa560695bb2b6
sparkidea25 <michaeldaralola123@gmail.com>
2022-05-15 17:57:27 +0100

    smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.22 <0.8.6;
+
+contract Election {
+    //Store Constructor
+    // Read Constructor
+    string public candidate;
+    // Constructor
+    function Elections () public {
+        candidate = "Candidate 1";
+
+    }
+
+}
